5a736d26cd904b916bcb0d7b49dbdd01db3256d2
gimbernat <gimbernat>
2018-11-12 20:31:06 +0100

    Initial commit

---
+++ src/main/java/Paquete/Persona.java
@@ -0,0 +1,58 @@
+package Paquete;
+
+import sun.util.calendar.LocalGregorianCalendar;
+
+import javax.persistence.*;
+
+@Entity
+
+public class Persona {
+
+    private long Dni;
+
+    private String nombre;
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    private String apellido;
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    private String email;
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    private String fecha_nacimiento;
+    public String getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+    public void setFecha_nacimiento(String fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "Dni=" + Dni +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", email='" + email + '\'' +
+                ", fecha_nacimiento='" + fecha_nacimiento + '\'' +
+                '}';
+    }
+
+}
